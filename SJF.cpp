ed8d487aad7e2de81c2856eac6d55bb669a1b495
Rohan-Pharswan <rohanpharswan11@gmail.com>
2025-08-17 21:59:29 +0530

    Update SJF.cpp

--- SJF.cpp
+++ SJF.cpp
@@ -1,47 +1,58 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef struct sjf{
-    pair<int,int> stbt;
+#include <stdio.h>
+struct sjf {
+    int arrival;
+    int burst;
     int com;
     int tat;
     int wt;
-} SJF;
-double cpu_uti=0;
-void compfinder(vector<SJF>&arr,int i,int prevcomp){
-    if(prevcomp>=arr[i].stbt.first){
-        arr[i].com=prevcomp+arr[i].stbt.second;
-        arr[i].tat=arr[i].com-arr[i].stbt.first;
-        arr[i].wt=arr[i-1].com-arr[i].stbt.first;
-        return;
+};
+double cpu_uti = 0.0;
+void compfinder(struct sjf arr[], int i, int prevcomp) {
+    if (prevcomp >= arr[i].arrival) {
+        arr[i].com = prevcomp + arr[i].burst;
+        arr[i].tat = arr[i].com - arr[i].arrival;
+        arr[i].wt  = prevcomp - arr[i].arrival;
+    } else {
+        cpu_uti += arr[i].arrival - prevcomp; // idle time
+        arr[i].com = arr[i].arrival + arr[i].burst;
+        arr[i].tat = arr[i].com - arr[i].arrival;
+        arr[i].wt  = 0;
     }
-    cpu_uti+=prevcomp-arr[i].stbt.first;
-    arr[i].com=arr[i].stbt.first+arr[i].stbt.second;
-    arr[i].tat=arr[i].com-arr[i].stbt.first;
-    arr[i].wt=0;
-
 }
-int main(){
+void sort_by_arrival(struct sjf arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j].arrival > arr[j + 1].arrival) {
+                struct sjf temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+int main() {
     int n;
-    cout<<"enter number of processes";
-    cin>>n;
-    vector<pair<int,int>> process(n);
-    cout<<"enter arriaval and burst time of n processes";
-    for(int i=0;i<n;i++){
-        cin>>process[i].first>>process[i].second;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    struct sjf p[n];
+    printf("Enter arrival and burst time of %d processes:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &p[i].arrival, &p[i].burst);
+        p[i].com = p[i].tat = p[i].wt = 0;
     }
-    sort(process.begin(),process.end());
-    vector<SJF>  p(n);
-    for(int i=0;i<n;i++){
-        p[i].stbt=process[i];
+    sort_by_arrival(p, n);
+    compfinder(p, 0, 0);
+    // Remaining processes
+    for (int i = 1; i < n; i++) {
+        compfinder(p, i, p[i - 1].com);
     }
-    compfinder(p,0,0);
-    for(int i=1;i<n;i++){
-        compfinder(p,i,p[i-1].com);
+    printf("Process\tArrival\tBurst\tCompletion\tTAT\tWT\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t\t%d\t%d\n",
+               i, p[i].arrival, p[i].burst,
+               p[i].com, p[i].tat, p[i].wt);
     }
-    cout<<"process  "<<"start time  "<<"burst time  "<<" complete Time  "<<" turn around time  "<<"waiting time  "<<endl;
-    for(int i=0;i<n;i++){
-        cout<<"p"<<i<<"\t\t"<<p[i].stbt.first<<"\t\t"<<p[i].stbt.second<<"\t\t"<<p[i].com<<"\t\t"<<p[i].tat<<"\t\t"<<p[i].wt<<endl;
-    }
-    cout<<"CPU Utilization: "<<(p[n-1].com-cpu_uti)/p[n-1].com*100<<"%"<<endl;
+    double utilization = ((p[n - 1].com - cpu_uti) / p[n - 1].com) * 100.0;
+    printf("CPU Utilization: %.2f%%\n", utilization);
     return 0;
 }
